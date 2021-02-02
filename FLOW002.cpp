d11b2248621c2e542985df7533cdfd9584ee0aa3
Sakshi Jain <62291788+im-sakshijain@users.noreply.github.com>
2021-02-02 22:32:10 +0530

    ques

---
+++ FLOW002.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases.
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int rem=a%b;
+        cout<<rem<<endl;
+    }
+	return 0;
+}
