d5e8a443e64d0dd3ae148e95bd64eab1a77f533b
Sakshi Jain <62291788+im-sakshijain@users.noreply.github.com>
2021-02-16 02:14:31 +0530

    Solved Ques

---
+++ No Succession.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==s[i+1]){
+                count++;
+            }
+        }
+        cout<<count<<endl;
+    }
+	// your code goes here
+	return 0;
+}
