0942daf015568825ca737aee3b68d86e5cf808fa
Sakshi Jain <62291788+im-sakshijain@users.noreply.github.com>
2021-03-09 18:22:40 +0530

    Solved Ques.cpp

--- Primality Test.cpp
+++ Primality Test.cpp
@@ -18,3 +18,33 @@ int main() {
     }
 	return 0;
 }
+//2nd soln
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,count = 0;
+        cin>>n;
+        if(n == 1 || n==0){
+            cout<<"no"<<endl;
+        }
+        else{
+        for(int i = 2;i<=sqrt(n);i++){
+            if(n%i == 0){
+                count++;
+            }
+        }
+        if(count>0){
+            cout<<"no"<<endl;
+        }
+        else{
+            cout<<"yes"<<endl;
+        }
+        }
+    }
+	// your code goes here
+	return 0;
+}
